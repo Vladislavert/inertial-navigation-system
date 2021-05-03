ad8ee41a4735042ff478db19639c8f8f86bc5e5a
Vladislavert <chuvarevanv@yandex.ru>
2021-05-03 15:39:37 +0300

    получение угла ориентации по гироскопу в matlab

--- scripts/getAngleGyroscope.m
+++ scripts/getAngleGyroscope.m
@@ -11,23 +11,49 @@ dataGyroscope = [dataSensors(1:end, indxGyro), dataSensors(1:end, indxGyro + 1),
 dataGyroscopeX = [dataSensors(1:end, indxGyro)];
 dataGyroscopeY = [dataSensors(1:end, indxGyro + 1)];
 dataGyroscopeZ = [dataSensors(1:end, indxGyro + 2)];
-hold off
-plot(dataSensors(1:end, end)/1000, dataGyroscopeZ)
-% проинтегриованные значения с гироскопа
-figure
-plot(dataSensors(1:end, end)/1000, cumtrapz(dataGyroscopeZ, rad2deg(dataSensors(1:end, end)/1000)))
+% hold off
+% lot(dataSensors(1:end, end)/1000, dataGyroscopeZ)
+% figure
+% значение угловой скорости по трём осям
+subplot(3, 1, 1)
+plot(dataSensors(1:end, end)/1000, rad2deg(dataGyroscopeX))
+title("Данные об угловых скоростях полученных с гироскопа")
+legend("Значение в градусах/сек по тангажу")
+subplot(3, 1, 2) 
+plot(dataSensors(1:end, end)/1000, rad2deg(dataGyroscopeY))
+legend("Значение в градусах/сек по крену")
+subplot(3, 1, 3)
+plot(dataSensors(1:end, end)/1000, rad2deg(dataGyroscopeZ))
+legend("Значение в градусах/сек по рысканью")
+
 
 % интегрирование методом Эйлера
 
 % начальное значение
-angleGyroscope(1) = deg2rad(dataSensors(1, indxOrient)); % угол рысканья
+angleGyroscopeX(1) = deg2rad(dataSensors(1, indxOrient + 1)); % угол тангажа
+angleGyroscopeY(1) = deg2rad(dataSensors(1, indxOrient + 2)); % угол крена
+angleGyroscopeZ(1) = deg2rad(dataSensors(1, indxOrient)); % угол рысканья
 for i = 2:size(dataSensors(1:end, 2))
-    angleGyroscope(i) = angleGyroscope(i - 1) + dataGyroscopeZ(i - 1)*(dataSensors(i, end) - dataSensors(i - 1, end))/1000;
-%     angleGyroscope(i) = angleGyroscope(i - 1) + 0.6*(dataSensors(i, end) - dataSensors(i - 1, end))/1000;
+    angleGyroscopeX(i) = angleGyroscopeX(i - 1) + dataGyroscopeX(i - 1)*(dataSensors(i, end) - dataSensors(i - 1, end))/1000;
+    angleGyroscopeY(i) = angleGyroscopeY(i - 1) + dataGyroscopeY(i - 1)*(dataSensors(i, end) - dataSensors(i - 1, end))/1000;
+    angleGyroscopeZ(i) = angleGyroscopeZ(i - 1) + dataGyroscopeZ(i - 1)*(dataSensors(i, end) - dataSensors(i - 1, end))/1000;
 end
 % отрисовка получившихся значений
 figure
-plot(dataSensors(1:end, end)/1000,  rad2deg(angleGyroscope))
+subplot(3, 1, 1)
+plot(dataSensors(1:end, end)/1000,  rad2deg(angleGyroscopeX))
+title("Данные об углах полученных с гироскопа")
+legend("Значение в градусах по тангажу")
+grid on
+subplot(3, 1, 2)
+plot(dataSensors(1:end, end)/1000,  rad2deg(angleGyroscopeY))
+legend("Значение в градусах по крену")
+grid on
+subplot(3, 1, 3)
+plot(dataSensors(1:end, end)/1000,  rad2deg(angleGyroscopeZ))
+legend("Значение в градусах по рысканью")
+grid on
+
 
 
 % hold on
@@ -36,5 +62,5 @@ plot(dataSensors(1:end, end)/1000,  rad2deg(angleGyroscope))
 % plot(dataMagnetometrX(1:end, 1), dataMagnetometrX(1:end, 2));
 
 % plot(dataMagnetometerX(1:end, 1), atan2(dataSensors(1:end, 1), dataSensors(1:end, 2)) * 180 / pi);
-grid on;
+
 % getAngleMagnetometer(dataMagnetometer)
