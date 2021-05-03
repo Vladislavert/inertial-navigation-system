bd279b95e2744c56e9191f0c82fe1ca45e57e701
Vladislavert <chuvarevanv@yandex.ru>
2021-05-03 22:14:14 +0300

    добавлена матрица поворотов из ССК в СНС

--- scripts/convertCoordBodyToWGS84.m
+++ scripts/convertCoordBodyToWGS84.m
@@ -1,3 +1,4 @@
+%% для получения информации введите команду >> help
 % /* ***************************************************************************************************   */
 % /* 	|Author: Vladislavert         |ssssssssssssssso++——+++osssssssssssssssssssssssssssssssssssssssss|  */
 % /* 	|e-mail: chuvarevan@mail.ru   |yyyysoooooo..   /   |    ./yyyyyyyyyyosshhhhhyyyyyyyyyyyyyyyyyyyy|  */
@@ -23,6 +24,13 @@
 % /*	║║║║║╚══╣╚═╝╠╝╚╦═╝║╚═╣╔╗║ ═ ║ysyysssssoooooooo++++:----::+oossysso++    oyyhyyhhyhyyhyhyyhyhyyyy|  */
 % /*	╚╝╚╝╚═══╩═══╩══╩══╩══╩╝╚╩═══╝yyysssssooooo++++++//:---:/++oosssssysss+: osyhhhyhhyyhyyyyyyyyyyyy|  */
 % /* ***************************************************************************************************   */
+% latitude(широта)
+% Longitude(долгота)
+% Altitude(высота)
+% Значение углов ориентации во время начальной выставки БИНС
+% Azimuth(Азимут)
+% Pitch(Тангаж)
+% Roll(Крен)
 
 % определение положение в WGS-84
 % 1. Накопить позицию по GPS(провести начальную выставку, в том числе БИНС)
@@ -32,6 +40,7 @@
 % 3. Перевод из стартовой СК в геоцентрическую СК(WGS-84) с помощью
 % сложения
 
+%%
 % положение в WGS-84
 % latitude(широта) = 55.814068
 % Longitude(долгота) = 37.50038
@@ -41,9 +50,24 @@
 % Pitch(Тангаж) = 0.6876
 % Roll(Крен) = -1.9878
 
+%%
+coordinateWGS = [55.814068, 37.50038, 210.7];
 
-coordinateWGS(55.814068, 37.50038, 210.7);
+%angl = [0*pi/180; 90*pi/180; 45*pi/180];
+angl = [0*pi/180; -1.46; 0.313];
 
+            R = [cos(angl(3))*cos(angl(2)), sin(angl(1))*sin(angl(3))*cos(angl(2))-cos(angl(1))*sin(angl(2)), cos(angl(1))*sin(angl(3))*cos(angl(2))+sin(angl(1))*sin(angl(2));...
+                 cos(angl(3))*sin(angl(2)), sin(angl(1))*sin(angl(3))*sin(angl(2))+cos(angl(1))*cos(angl(2)), cos(angl(1))*sin(angl(3))*sin(angl(2))-sin(angl(1))*cos(angl(2));...
+                 -sin(angl(3))                                       , sin(angl(1))*cos(angl(3))                                        , cos(angl(1))*cos(angl(3))] ;
+Vector = [0;0;4.28];
+Rotation = R*Vector
+
+plot3([0,Vector(1)],[0,Vector(2)],[0,Vector(3)])
+hold on
+plot3([0,Rotation(1)],[0,Rotation(2)],[0,Rotation(3)])
+xlabel('x')
+ylabel('z')
+zlabel('y')
 
 
 
